f58515a549ceb8f2d0d90b4fc06eabe6c7d80696
fentyIsaac <piggottfentyi@students.westerntc.edu>
2024-12-10 01:52:10 -0600

    Finished Project

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,10 +1,14 @@
 const {authenticateUser} = require("./auth.data.service")
+const {getToken, decodeToken} = require("./auth.helpers");
 
 exports.loginHandler = async (req, res, next) => {
   try{
     const {email,password} = req.body;
     const user = await authenticateUser(email, password);
-    // TODO: set JWT
+    
+    const token = getToken(user.id, user.roleId);
+    res.setHeader("Authorization","Bearer " + token);
+
     res.json(user);
   }catch(err){
     if(err.message.startsWith("Invalid password") || err.message.startsWith("User not found")){
@@ -15,11 +19,29 @@ exports.loginHandler = async (req, res, next) => {
   }
 }
 
-// Since we are using JWT tokens instead of cookies, there is no way to log out!
 exports.logoutHandler = async (req, res, next) => {
   try{
     res.json({message:"TODO: log out"});
   }catch(err){
     next(err);
+  }
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  const { authorization } = req.headers
+  
+  try{
+    const token = authorization.substring('Bearer '.length);
+    const {userId, roleId, exp} = decodeToken(token);
+    const currentTimeInSeconds = Date.now()/1000;
+    if(exp < currentTimeInSeconds){
+      throw new Error("Token expired");
+    }else{
+      // TODO: you could use the auth.data.service to see of the user is still active
+      next();
+      return;
+    } 
+  }catch(err){
+    res.status(403).json({message:"Not Authenticated"})
   }
 }
--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {loginHandler, logoutHandler} = require("./auth.controller")
+const {loginHandler, logoutHandler, isLoggedIn} = require("./auth.controller")
 
 router.post("/login", loginHandler);
 router.get("/logout", logoutHandler);
+
+router.get("/loginCheck", isLoggedIn,  (req, res, next) => {
+  res.json({message: "Looks like your logged in!"})
+})
 
 module.exports = router;
