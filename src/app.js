906dbdb92f2319321887bbe3a23eb5f5c30d6d0e
fentyIsaac <piggottfentyi@students.westerntc.edu>
2024-12-12 11:57:37 -0600

    removed commit

--- src/app.js
+++ src/app.js
@@ -6,35 +6,7 @@ const app = express();
 app.use(helmet());
 app.use(express.json());
 
-// If using front-end we would need a CORS package.
-// Thus allowing cross-origin resourfce sharing
 
-/*
-
-Would look like this: It is a middleware in itself
-
-app.use(cors());
-
-Also:
-For specefic protection of routes: like users you would use:
-app.use("/auth", cors(), require("./auth/auth.routes"));
-*/
-
-
-/*
-Config of CORS:
-
-const corsConfig = {
-
-origin: 'http://localhost:3000 (this is our front end domain);
-methods: 'GET, POST, PUT, DELETE' (this allows only specific methods to be ran )
-allowedHeaders: 'Content-Type, Authorization' (allows only certian headers)
-
-}
-
-
-app.use(cors(corsConfig));
-*/
 
 
 const corsOptions = {
